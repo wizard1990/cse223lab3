bf3eb8c8a214eebc2f05a2887fbc642964b6b0a4
tao <popacai@gmail.com>
2014-04-27 23:21:40 -0700

    FindMaxCLock func done

--- triblab/attFunc.go
+++ triblab/attFunc.go
@@ -1,7 +1,7 @@
 package triblab
 
 import (
-	//"fmt"
+	"fmt"
 	"strconv"
 	"strings"
 	"trib"
@@ -20,8 +20,49 @@ func AddClock(clock uint64, str string) string {
 	str = clock_str + "," + str
 	return str
 }
-func FindLargestClock(l1 *trib.List, l2 *trib.List, l3 *trib.List) (uint64, *trib.List, string) {
-	//max_collect := make(map[uint64]string)
 
-	return 0, nil, ""
+func DeleteClockList(l *trib.List) *trib.List {
+	return nil
+}
+func FindLargestClock(l1 *trib.List, l2 *trib.List, l3 *trib.List) (uint64, *trib.List, string) {
+	L := make([]*trib.List, 3)
+	L[0] = l1
+	L[1] = l2
+	L[2] = l3
+
+	var max_count uint64
+	max_count = 0
+	max_collect := make(map[uint64]string)
+	for i, _ := range L {
+		for _, v := range L[i].L {
+			c, _ := SplitClock(v)
+			if c >= max_count {
+				max_count = c
+			}
+
+			//Check it if in the max_collection
+			value, ok := max_collect[c]
+			if !ok {
+				max_collect[c] = v
+			} else {
+				if value != v {
+					fmt.Println("Error!", value, " != ", v)
+				}
+			}
+		}
+	}
+
+	//rebuild the tribList
+	new_triblist := trib.List{}
+	new_triblist.L = make([]string, len(max_collect))
+
+	j := 0
+	for _, v := range max_collect {
+		//fmt.Println(i, v)
+		new_triblist.L[j] = v
+		j++
+	}
+
+	_, message := SplitClock(max_collect[max_count])
+	return max_count, &new_triblist, message
 }
